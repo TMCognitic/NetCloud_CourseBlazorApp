b3f732e7e5430dba5a6d6832770e1352c79f06ee
Morre Thierry <thierry.morre@cognitic.be>
2024-01-17 14:21:10 +0100

    Ajoutez des fichiers projet.

---
+++ CourseBlazorApp/Pages/EventCallbackSample/Child.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CourseBlazorApp.Pages.EventCallbackSample
+{
+    public partial class Child
+    {
+        [Parameter]
+        public EventCallback<int> OnRandomGenerated { get; set; }
+
+
+        private int Number { get; set; }
+
+        public void OnClick()
+        {
+            Number = Random.Shared.Next();
+            OnRandomGenerated.InvokeAsync(Number);
+        }
+    }
+}
---
+++ CourseBlazorApp/Pages/EventCallbackSample/Parent.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace CourseBlazorApp.Pages.EventCallbackSample
+{
+    public partial class Parent
+    {
+        private List<int> Items { get; set; } = new List<int>();
+
+
+        private void OnNewNumber(int number)
+        {
+            Items.Add(number);
+        }
+    }
+}
