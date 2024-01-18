b29ab2ffef3a3b76cf23eb145b45e18d6b8af00b
Morre Thierry <thierry.morre@cognitic.be>
2024-01-18 14:38:15 +0100

    Ajout Gestion de formulaire

---
+++ CourseBlazorApp/Models/Forms/CreateContactForm.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseBlazorApp.Models.Forms
+{
+    public class CreateContactForm
+    {
+        [Required]
+        public string Nom { get; set; } = default!;
+        [Required]
+        public string Prenom { get; set; } = default!;
+        [Required]
+        public DateTime Anniversaire { get; set; } = DateTime.Now;
+    }
+}
---
+++ CourseBlazorApp/Pages/FormSample/FormSample.razor.cs
@@ -0,0 +1,21 @@
+﻿using CourseBlazorApp.Models.Forms;
+
+namespace CourseBlazorApp.Pages.FormSample
+{
+    public partial class FormSample
+    {
+        private string? Content { get; set; }
+
+        private CreateContactForm Form { get; set; } = default!;
+
+        protected override void OnInitialized()
+        {
+            Form = new CreateContactForm();
+        }
+
+        public void Save()
+        {
+            Content = "Saved!!";
+        }
+    }
+}
